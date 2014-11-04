5375cfd3e696c7d371dfec4619b04d1fbac52005
joshua.baird <joshua.baird@6a2c2425-dea3-90a6-7eab-ce57b7db7bff>
2014-11-04 23:09:22 +0000

    - added more UI classes. I think I got them all.
    git-svn-id: https://github.com/joshbaird/LessonPlanOrganizer/trunk@8 6a2c2425-dea3-90a6-7eab-ce57b7db7bff

---
+++ LessonPlanOrganizer/UI/LessonUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LessonPlanOrganizer
+{
+    public partial class LessonUI : Form
+    {
+        public LessonUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
