629d10213b88872e2f4f95754e225e4413f55468
joshua.baird <joshua.baird@6a2c2425-dea3-90a6-7eab-ce57b7db7bff>
2014-11-05 14:43:42 +0000

    - added templates for all the entity objects...
    git-svn-id: https://github.com/joshbaird/LessonPlanOrganizer/trunk@9 6a2c2425-dea3-90a6-7eab-ce57b7db7bff

--- LessonPlanOrganizer/Entity/LessonPlanYear.cs
+++ LessonPlanOrganizer/Entity/LessonPlanYear.cs
@@ -7,5 +7,75 @@ namespace LessonPlanOrganizer
 {
     class LessonPlanYear
     {
+        public LessonPlanYear()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public LessonPlanYear createLessonPlanYear(DateTime startDate, DateTime endDate)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public LessonPlanYear displayLessonPlanYear(DateTime startDate, DateTime endDate)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public LessonPlanYear selectLessonPlanYear(DateTime startDate, DateTime endDate)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public LessonPlanYear editLessonPlanYear(DateTime startDate, DateTime endDate)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public LessonPlanYear deleteLessonPlanYear(DateTime startDate, DateTime endDate)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public LessonPlanYear saveLessonPlanYear(DateTime startDate, DateTime endDate)
+        {
+            return this;
+        }
     }
 }
--- LessonPlanOrganizer/Entity/Report.cs
+++ LessonPlanOrganizer/Entity/Report.cs
@@ -7,5 +7,43 @@ namespace LessonPlanOrganizer
 {
     class Report
     {
+        private Report()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <param name="requredTime"></param>
+        /// <returns></returns>
+        public Report generateSubStatsReport(String subject, DateTime startDate, DateTime endDate, int requredTime) 
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reportType"></param>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public Report generateLessonPlanReport(String reportType, DateTime startDate, DateTime endDate)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="r"></param>
+        public void printReport(Report r)
+        {
+
+        }
     }
 }
