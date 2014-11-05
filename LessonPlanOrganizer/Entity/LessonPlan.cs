39ca9e8b3db1ed4988fb9f7627b96e1a76ed851d
Joshua Baird <JBaird@Alpine-la.com>
2014-11-05 06:43:42 -0800

    - added templates for all the entity objects...

--- LessonPlanOrganizer/Entity/LessonPlan.cs
+++ LessonPlanOrganizer/Entity/LessonPlan.cs
@@ -7,5 +7,85 @@ namespace LessonPlanOrganizer
 {
     class LessonPlan
     {
+        public LessonPlan()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Year"></param>
+        /// <param name="subject"></param>
+        /// <param name="lessonPlanDate"></param>
+        /// <param name="lessonPlanTime"></param>
+        /// <returns></returns>
+        public LessonPlan createLessonPlan(LessonPlanYear Year, String subject, DateTime lessonPlanDate, DateTime lessonPlanTime)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="lessonPlanDate"></param>
+        /// <param name="lessonPlanTime"></param>
+        /// <returns></returns>
+        public LessonPlan selectLessonPlan(String subject, DateTime lessonPlanDate, DateTime lessonPlanTime)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="lessonPlanDate"></param>
+        /// <param name="lessonPlanTime"></param>
+        /// <returns></returns>
+        public LessonPlan displayLessonPlan(String subject, DateTime lessonPlanDate, DateTime lessonPlanTime)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Year"></param>
+        /// <param name="subject"></param>
+        /// <param name="lessonPlanDate"></param>
+        /// <param name="lessonPlanTime"></param>
+        /// <returns></returns>
+        public LessonPlan saveLessonPlan(LessonPlanYear Year, String subject, DateTime lessonPlanDate, DateTime lessonPlanTime)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="lessonPlanDate"></param>
+        /// <param name="lessonPlanTime"></param>
+        /// <returns></returns>
+        public LessonPlan editLessonPlan (String subject, DateTime lessonPlanDate, DateTime lessonPlanTime)
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="lessonPlanDate"></param>
+        /// <param name="lessonPlanTime"></param>
+        /// <returns></returns>
+        public Boolean deleteLessonPlan(String subject, DateTime lessonPlanDate, DateTime lessonPlanTime)
+        {
+            return true;
+        }
+
+
     }
 }
