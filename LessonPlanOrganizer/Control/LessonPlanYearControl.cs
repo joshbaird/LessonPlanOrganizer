440f9686205bfe9e61ce724065d2a4f7b10effac
prevren986 <prevren986@yahoo.com>
2014-12-02 12:38:12 -0800

    --corrected DB reconnect issue, mine kept recreating the DB file because the sqlite variable wasn't completly a string. Added toString() at the end of the Environment Variable call.

--- LessonPlanOrganizer/Control/LessonPlanYearControl.cs
+++ LessonPlanOrganizer/Control/LessonPlanYearControl.cs
@@ -16,7 +16,7 @@ namespace LessonPlanOrganizer
         private LessonPlanYearControl()
         {
             // TODO replace dateTimes with actual dates and times.
-            _dbWrapper = new DataBaseWrapper(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "lesson_db.sqlite");
+            _dbWrapper = new DataBaseWrapper(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + "lesson_db.sqlite");
             fillLessonPlanYearFromDb();
             
             EventsControl.AddNewSubject += (o, e) =>
